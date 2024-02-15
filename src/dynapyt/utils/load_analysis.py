9b84494c66d9bed46bc0d7675e05a91e1d45f15f
Nima Modares Gorji <nima.mg@gmail.com>
2024-02-15 12:32:22 -0800

    Refactor: Move loading class from path to a new util function

--- src/dynapyt/utils/load_analysis.py
+++ src/dynapyt/utils/load_analysis.py
@@ -1,22 +1,13 @@
-import importlib
 from typing import List, Any
 from ..analyses.BaseAnalysis import BaseAnalysis
+from .load_class_from_path import load_class_from_path
 
 
 def load_analyses(analyses: List[Any]) -> List[BaseAnalysis]:
     res_analyses = []
     for ana in analyses:
         if isinstance(ana, str):
-            conf = None
-            if ":" in ana:
-                ana, conf = ana.split(":")
-            module_parts = ana.split(".")
-            module = importlib.import_module(".".join(module_parts[:-1]))
-            class_ = getattr(module, module_parts[-1])
-            if conf is not None:
-                res_analyses.append(class_(conf))
-            else:
-                res_analyses.append(class_())
+            res_analyses.append(load_class_from_path(ana))
         elif isinstance(ana, BaseAnalysis):
             res_analyses.append(ana)
         else:
